fed717a249c83b0c594ab34f5da8bb0c53097f91
acrdlph <achill@convergent.cx>
2019-01-25 13:56:44 +0100

    buy not working yet?

--- test/BondedFungibleToken.js
+++ test/BondedFungibleToken.js
@@ -21,7 +21,7 @@ contract('BondedFungibleToken', (accounts) => {
   let bft, mERC20;
 
   before(async () => {
-    mERC20 = await deployMockERC20(toWei('1', 'ether')); // why is the argument ether here? this is a generic ERC20, so the supply should just be a number
+    mERC20 = await deployMockERC20(toWei('1', 'ether')); // why is the argument expressed in wei here? this is a generic ERC20, so the supply should just be a number
     expect(mERC20.address).to.exist;
 
     bft = await BFT.new()
@@ -30,17 +30,17 @@ contract('BondedFungibleToken', (accounts) => {
   });
 
   it('Returns expected parameters on initialization', async () => {
-    const ppm = await bft.PPM();
-    const reserveAsset = await bft.reserveAsset();
-    const name = await bft.name();
-    const owner = await bft.owner();
+    const retPpm = await bft.PPM();
+    const retReserveAsset = await bft.reserveAsset();
+    const retName = await bft.name();
+    const retOwner = await bft.owner();
     expect(
-      ppm.toNumber()
+      retPpm.toNumber()
     ).to.equal(1000000);
     expect(mERC20.address).to.exist;
-    expect(reserveAsset).to.equal(mERC20.address);
-    expect(name).to.equal('Achill');
-    expect(owner).to.equal(accounts[0]);
+    expect(retReserveAsset).to.equal(mERC20.address);
+    expect(retName).to.equal('Achill');
+    expect(retOwner).to.equal(accounts[0]);
 
     const reserve = await bft.reserve();
     expect(
@@ -54,9 +54,16 @@ contract('BondedFungibleToken', (accounts) => {
   });
 
 
+  it('Allows buying token', async () => {
+    await bft.buy(1000, 0, 0);
+    const retHeldContributions = await bft.heldContributions();
+    expect(heldContributions.toString()
+    ).to.equal('1000');
+  });
+
   // it('Tests some view functions', async () => {
   //   const numTokens = toBN(2100).mul(PT_SCALE);
-  //   const cost = await lp.cost(numTokens.toString());
+  //   const cost = await bft.cost(numTokens.toString());
   //   console.log(fromWei(cost.div(PT_PRECISION).toString()));
   // });
 });
