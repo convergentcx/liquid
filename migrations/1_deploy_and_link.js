39d754524e91a2ecde33f54d56c5e7f169210ce3
acrdlph <achill@convergent.cx>
2019-01-25 13:05:33 +0100

    running out of gas on BFT deployment during test

--- migrations/1_deploy_and_link.js
+++ migrations/1_deploy_and_link.js
@@ -1,28 +1,6 @@
-const ExponentLib = artifacts.require('ExponentLib');
-const FixidityLib = artifacts.require('FixidityLib');
-const LogarithmLib = artifacts.require('LogarithmLib');
-const Migrations = artifacts.require('Migrations');
-const Polynomial = artifacts.require('Polynomial');
+const BondedFungibleToken = artifacts.require('BondedFungibleToken');
 
 module.exports = async deployer => {
   /// Migrations
-  await deployer.deploy(Migrations);
-
-  /// FixidityLib
-  await deployer.deploy(FixidityLib);
-
-  /// LogarithmLib
-  await deployer.link(FixidityLib, LogarithmLib);
-  await deployer.deploy(LogarithmLib);
-
-  /// ExponentLib
-  await deployer.link(FixidityLib, ExponentLib);
-  await deployer.link(LogarithmLib, ExponentLib);
-  await deployer.deploy(ExponentLib);
-
-  /// Polynomial
-  await deployer.link(FixidityLib, Polynomial);
-  await deployer.link(LogarithmLib, Polynomial);
-  await deployer.link(ExponentLib, Polynomial);
-  await deployer.deploy(Polynomial, 1, 1, 1, 1);
+  await deployer.deploy(BondedFungibleToken);
 }
