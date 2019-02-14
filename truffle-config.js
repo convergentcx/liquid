0d8dfbda86335dcc4e963297261304b3b03702f4
Logan Saether <lsaether@protonmail.com>
2019-02-14 13:41:41 +0100

    latest

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
+const NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker")
 
 const { M_PK, PK, PK2 } = process.env;
 // console.log(pk);
@@ -15,17 +16,30 @@ module.exports = {
       network_id: '*'
     },
     rinkeby: {
-      provider: () => new HDWalletProvider([PK, PK2], 'https://rinkeby.infura.io/v3/7121204aac9a45dcb9c2cc825fb85159', 0, 2),
+      provider: () => {
+        const wallet = new HDWalletProvider([PK, PK2], 'https://rinkeby.infura.io/v3/7121204aac9a45dcb9c2cc825fb85159', 0, 2)
+        const nonceTracker = new NonceTrackerSubprovider();
+        wallet.engine._providers.unshift(nonceTracker);
+        nonceTracker.setEngine(wallet.engine);
+        return wallet;
+      },
       network_id: '4',
       gas: 5000000,
       gasPrice: 3e9,
     },
     mainnet: {
-      provider: () => new HDWalletProvider([M_PK], 'https://mainnet.infura.io/v3/372037f46312422895436182560f5a87'),
+      provider: () => {
+        const wallet = new HDWalletProvider([M_PK], 'https://neatly-tolerant-coral.quiknode.io/73b04107-89ee-4261-9a8f-3c1e946c17b2/CyYMMeeGTb-EeIBHGwORaw==/');
+        const nonceTracker = new NonceTrackerSubprovider();
+        wallet.engine._providers.unshift(nonceTracker);
+        nonceTracker.setEngine(wallet.engine);
+        return wallet;
+      },
       network_id: '1',
-      gas: 20000,
-      gasPrice: 2e9,
-    }
+      gas: 4000000,
+      gasPrice: 6e9,
+      timeoutBlocks: 1000000, // dont timeout
+    },
   },
   compilers: {
     solc: {
